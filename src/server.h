9122f58f4e1e3634d52e70503fd371797d4206a2
micallef25 <30693668+micallef25@users.noreply.github.com>
2019-11-03 15:17:45 -0500

    Update server.h

--- src/server.h
+++ src/server.h
@@ -11,13 +11,18 @@ public:
 
 	//
 	int get_packet(unsigned char* buffer);
-
+    
+	
+    // how many packets have we distributed
     int packet_counter;
-
+    
+    // how many packets did we break our file into?
     int table_size;
-
+	
+    // data read in from a file
     unsigned char* input_data;
-
+	
+    // table of pointers that points to an array where your packets live
     unsigned char** table;
 
 protected:
